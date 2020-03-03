4a5c6cb360990f6eb28488e7cc20e8f051d0ff76
taroj khan <tarujkhan@yahoo.com>
2020-03-03 19:22:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -5,43 +5,43 @@ end
 def create_an_array
   array = ["Hello","Taroj", "Ahmed", "Khan"]
 end
-puts create_an_empty_array.inspect
+puts create_an_array.inspect
 
 def add_element_to_end_of_array(array, element)
   array << "arrays!"
 end
-puts create_an_empty_array.inspect
+puts create_an_array.inspect
 
 def add_element_to_start_of_array(array, element)
   array.unshift("wow")
 end
-puts create_an_empty_array.inspect
+puts create_an_array.inspect
 
 def remove_element_from_end_of_array(array)
-  "Khan" = create_an_empty_array.pop
+  "Khan" = create_an_array.pop
 end
-#puts create_an_empty_array.inspect
+puts create_an_array.inspect
 puts "Khan"
 
 
 def remove_element_from_start_of_array(array)
-  "Hello" = create_an_empty_array.shift
+  "Hello" = create_an_array.shift
 end
-#puts create_an_empty_array.inspect
+puts create_an_array.inspect
 puts "Hello"
 
 
 def retrieve_element_from_index(array, index_number)
   array[2]
 end
-puts create_an_empty_array.inspect
+puts create_an_array.inspect
 
 def retrieve_first_element_from_array(array)
   array[0]
 end
-puts create_an_empty_array.inspect
+puts create_an_array.inspect
 
 def retrieve_last_element_from_array(array)
   array[-1]
 end
-puts create_an_empty_array.inspect
+puts create_an_array.inspect
