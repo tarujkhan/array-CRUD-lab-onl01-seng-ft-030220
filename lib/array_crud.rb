b043749401ba7511921f15dcf22c203ca3eec118
taroj khan <tarujkhan@yahoo.com>
2020-03-03 18:52:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,29 +15,29 @@ puts create_an_empty_array.inspect
 def add_element_to_start_of_array(array, element)
   my_array.unshift("How are you?")
 end
-puts add_element_to_start_of_array.inspect
+puts create_an_empty_array.inspect
 
 def remove_element_from_end_of_array(array)
   my_array.pop("Khan")
 end
-puts add_element_to_start_of_array.inspect
+puts create_an_empty_array.inspect
 
 def remove_element_from_start_of_array(array)
   my_array.shift("Taroj")
 end
-puts add_element_to_start_of_array.inspect
+puts create_an_empty_array.inspect
 
 def retrieve_element_from_index(array, index_number)
   my_array[]
 end
-puts add_element_to_start_of_array.inspect
+puts create_an_empty_array.inspect
 
 def retrieve_first_element_from_array(array)
   my_array[0]
 end
-puts add_element_to_start_of_array.inspect
+puts create_an_empty_array.inspect
 
 def retrieve_last_element_from_array(array)
   my_array[-1]
 end
-puts add_element_to_start_of_array.inspect
+puts create_an_empty_array.inspect
