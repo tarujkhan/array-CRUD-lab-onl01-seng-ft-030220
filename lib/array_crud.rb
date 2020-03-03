7077fc7ac10036be2060add9b1c2b366ce1b70ec
taroj khan <tarujkhan@yahoo.com>
2020-03-03 18:54:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,43 +1,43 @@
 def create_an_empty_array
-  my_array = []
+  array = []
 end
 
 def create_an_array
-  my_array = ["Hello","Taroj", "Ahmed", "Khan"]
+  array = ["Hello","Taroj", "Ahmed", "Khan"]
 end
 puts create_an_empty_array.inspect
 
 def add_element_to_end_of_array(array, element)
-  my_array << "Welcome"
+  array << "Welcome"
 end
 puts create_an_empty_array.inspect
 
 def add_element_to_start_of_array(array, element)
-  my_array.unshift("How are you?")
+  array.unshift("How are you?")
 end
 puts create_an_empty_array.inspect
 
 def remove_element_from_end_of_array(array)
-  my_array.pop("Khan")
+  array.pop("Khan")
 end
 puts create_an_empty_array.inspect
 
 def remove_element_from_start_of_array(array)
-  my_array.shift("Taroj")
+  array.shift("Taroj")
 end
 puts create_an_empty_array.inspect
 
 def retrieve_element_from_index(array, index_number)
-  my_array[]
+  array[2]
 end
 puts create_an_empty_array.inspect
 
 def retrieve_first_element_from_array(array)
-  my_array[0]
+  array[0]
 end
 puts create_an_empty_array.inspect
 
 def retrieve_last_element_from_array(array)
-  my_array[-1]
+  array[-1]
 end
 puts create_an_empty_array.inspect
