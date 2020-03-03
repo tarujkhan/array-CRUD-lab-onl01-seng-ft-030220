470d60f7751b81e2a7ae22d85ddfcead61886fbe
taroj khan <tarujkhan@yahoo.com>
2020-03-03 19:30:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,14 +18,14 @@ end
 puts create_an_array.inspect
 
 def remove_element_from_end_of_array(array)
-  create_an_array.pop
+  array.pop
 end
 puts create_an_array.inspect
 
 
 
 def remove_element_from_start_of_array(array)
-  create_an_array.shift
+  array.shift
 end
 puts create_an_array.inspect
 
